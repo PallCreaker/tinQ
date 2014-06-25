6b7773dc5046719561c7ba4ebf3da7f505aadfd7
kazumasa Fujita <pallcreaker@gmail.com>
2014-06-25 15:23:59 +0900

    a liitle fiexed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,10 @@ class UsersController < ApplicationController
 
   def show
   end
+
+  private
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
 end
