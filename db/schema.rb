e7c1638e35e7d8db72aec15d1f69581b30854f7e
iMac for local <pallcreaker@gmail.com>
2014-06-30 18:17:00 +0900

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140625071027) do
+ActiveRecord::Schema.define(version: 20140630090615) do
+
+  create_table "brands", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "categories", force: true do |t|
     t.string   "name"
