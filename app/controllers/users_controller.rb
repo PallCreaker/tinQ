50f2f265ccfb7398d0c15f6adc6be70ee4a13051
wataori <ropifuku@gmail.com>
2014-06-30 19:59:30 +0900

    attr_accessible的なのかいた

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     redirect_to(root_path) unless current_user?(@user)
   end
+
+  def user_params
+    params.require(:user).permit(:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip, :name)
+  end
 end
