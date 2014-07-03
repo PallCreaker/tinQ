2410168edc4af3a285d0788dd01774a251c97435
iMac for local <pallcreaker@gmail.com>
2014-07-04 02:03:34 +0900

    refs #6 photo migrate work

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140630090615) do
+ActiveRecord::Schema.define(version: 20140703165951) do
 
   create_table "brands", force: true do |t|
     t.string   "name"
@@ -32,10 +32,14 @@ ActiveRecord::Schema.define(version: 20140630090615) do
     t.integer  "product_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   create_table "products", force: true do |t|
-    t.string   "good_name"
+    t.string   "goods_name"
     t.integer  "brand_id"
     t.datetime "created_at"
     t.datetime "updated_at"
