b6f306f88f0060df948607047e30db42f38726a1
iMac for local <pallcreaker@gmail.com>
2014-07-04 21:52:48 +0900

    refs #6 削除したかのカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140703165951) do
+ActiveRecord::Schema.define(version: 20140704125007) do
 
   create_table "brands", force: true do |t|
     t.string   "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20140703165951) do
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
+    t.integer  "is_deleted",         default: 0, null: false
   end
 
   create_table "products", force: true do |t|
