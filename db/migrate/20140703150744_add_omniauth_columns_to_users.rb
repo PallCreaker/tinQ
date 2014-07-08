ab42a395d4e1a93657a035cc058ad24a18823ab0
wataori <ropifuku@gmail.com>
2014-07-08 11:33:47 +0900

    fix #3 2回目レビュー修正

--- db/migrate/20140703150744_add_omniauth_columns_to_users.rb
+++ db/migrate/20140703150744_add_omniauth_columns_to_users.rb
@@ -2,5 +2,7 @@ class AddOmniauthColumnsToUsers < ActiveRecord::Migration
   def change
     add_column :users, :uid,      :string, null: false, default: ""
     add_column :users, :provider, :string, null: false, default: ""
+
+    add_index :users, [:uid, :provider], unique: true
   end
 end
