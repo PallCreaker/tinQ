0a28ec477847fb8df197e66665920dcf886845a5
wataori <ropifuku@gmail.com>
2014-07-12 22:56:57 +0900

    #12 productモデルにimageモデルとの関連を追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
   belongs_to :brand
   has_many :posts
+  has_many :images
 
   validates :brand_id, presence: true, numericality: { only_integer: true }
   validates :goods_name, presence: { message: 'なにか入力して下さい'}
