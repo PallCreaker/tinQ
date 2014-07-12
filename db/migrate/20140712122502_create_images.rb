85d9bce2cc4e34031739624523bc5ba01b528fc6
wataori <ropifuku@gmail.com>
2014-07-12 23:00:26 +0900

    #12 qynqynテーブルにpost_id追加と非null制約、imageテーブルのproduct_idにも非NULL制約

--- db/migrate/20140712122502_create_images.rb
+++ db/migrate/20140712122502_create_images.rb
@@ -1,7 +1,7 @@
 class CreateImages < ActiveRecord::Migration
   def change
     create_table :images do |t|
-      t.integer :product_id
+      t.integer :product_id, null: false
       t.attachment :image
     end
   end
