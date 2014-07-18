03184d0b83952f9ab0aa8997ac942ceca4e2d436
tinq from aws <kazumasa.fuji@gmail.com>
2014-07-18 16:19:00 +0000

    unicornの環境設定完了

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,28 +1,37 @@
 # coding: utf-8
+#
+#------------------------------------------------------------------------------
+## 起動確認 コマンド
+## bundle exec unicorn_rails -E production -c config/unicorn.rb
+##------------------------------------------------------------------------------
+WORKING_DIR = "/home/ec2-user/public_html/tinQ"
 
 # ワーカーの数
 worker_processes 2
+working_directory "#{WORKING_DIR}"
 
 # ソケット Nginxの設定と合わせる
-# listen "#{RAILS_ROOT}/tmp/unicorn.sock", :backlog => 64
 # # listen 8080, :tcp_nopush
-listen "#{RAILS_ROOT}/tmp/unicorn.sock"
+listen "/tmp/unicorn.sock"
 
+# nuke workers after 30 seconds instead of 60 seconds (the default)
+timeout 60 # タイムアウトは１分
 
-pid "#{RAILS_ROOT}/log/unicorn.pid"
+# feel free to point this anywhere accessible on the filesystem
+pid "#{WORKING_DIR}/log/unicorn.pid"
 
 # log.  By default, the Unicorn Logger will write to stderr.
 # stderr_path File.expand_path('unicorn.log', File.dirname(__FILE__) + '/../log')
 # stdout_path File.expand_path('unicorn.log', File.dirname(__FILE__) + '/../log')
-stderr_path "#{RAILS_ROOT}/log/unicorn.log"
-stdout_path "#{RAILS_ROOT}/log/unicorn.log"
+stderr_path "#{WORKING_DIR}/log/unicorn.log"
+stdout_path "#{WORKING_DIR}/log/unicorn.log"
 
 # ダウンタイムをなくす
 preload_app true
 
 
 before_fork do |server, worker|
-  old_pid = "#{RAILS_ROOT}/log/unicorn.pid.oldbin"
+  old_pid = "#{WORKING_DIR}/log/unicorn.pid.oldbin"
   if File.exists?(old_pid) && server.pid != old_pid
     begin
       Process.kill("QUIT", File.read(old_pid).to_i)
