c4010a24bf17a8e0e386e91cfc103c0d6f6291eb
kazumasa Fujita <pallcreaker@gmail.com>
2014-07-21 14:24:18 +0900

    cap 設定ファイルを追加

---
+++ config/deploy/recipe.class.rb
@@ -0,0 +1,26 @@
+class Recipe
+  
+  def initialize(recipe_id)
+    @recipe_id = recipe_id
+  end
+
+  def display_id
+    puts @recipe_id
+  end
+
+  def get_title
+    
+  end
+
+  def get_food
+    
+  end
+
+  def get_step
+    
+  end
+
+  def output
+    
+  end
+end
